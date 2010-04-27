0c5b45ec1c69db61a20d58e2ea2ec2522c8c4a32
marc.fornos <marc.fornos@0eba084e-8743-11dd-9299-37fd5bf03096>
2010-04-27 17:46:52 +0000

    Some nice colors...

--- src/main/org/mafc/blob/BlobViewer.java
+++ src/main/org/mafc/blob/BlobViewer.java
@@ -3,8 +3,11 @@ package org.mafc.blob;
 import dynsim.ui.AnimationFrame;
 
 public class BlobViewer {
+	private static final int HEIGHT = 300;
+	private static final int WIDTH = 500;
+
 	public static void main(String[] args) {
-		BlobScene s = new BlobScene(500, 300);
+		BlobScene s = new BlobScene(WIDTH, HEIGHT);
 		s.insert(new Blob(8, new Vector2D(10, 12), new Vector2D(0, 1.2), 0.8));
 		s.insert(new Blob(7, new Vector2D(60, 60), new Vector2D(0, 0), 1.875));
 		s.insert(new Blob(6, new Vector2D(100, 10), new Vector2D(1.2, 1.2), 2.875));
@@ -17,6 +20,6 @@ public class BlobViewer {
 		s.insert(new Blob(10, new Vector2D(146, 55), new Vector2D(0., 0.15), 0.88));
 		s.insert(new Blob(5, new Vector2D(155, 52), new Vector2D(0., 0.15), 0.88));
 
-		new AnimationFrame("Blob Scene [" + s + "]", s, false);
+		new AnimationFrame("Blob Scene [" + s + "]", s, WIDTH, HEIGHT, false);
 	}
 }
